0b524ee8ad5ab407d436f95c8b2249cc33035cae
Nam Chu Hoai <nambrot@googlemail.com>
2015-03-27 20:59:12 +0200

    Do not assign the result of the proc call to key
    Otherwise, you end up having the same key for the whole collection.

--- lib/jbuilder_cache_multi/jbuilder_ext.rb
+++ lib/jbuilder_cache_multi/jbuilder_ext.rb
@@ -7,30 +7,30 @@ JbuilderTemplate.class_eval do
   # json.cache_collection! @people, expires_in: 10.minutes do |person|
   #   json.partial! 'person', :person => person
   # end
-  def cache_collection!(collection, options = {}, &block)    
+  def cache_collection!(collection, options = {}, &block)
     if @context.controller.perform_caching
-      keys_to_collection_map = _keys_to_collection_map(collection, options)  
+      keys_to_collection_map = _keys_to_collection_map(collection, options)
 
       if ::Rails.cache.respond_to?(:fetch_multi)
         results = ::Rails.cache.fetch_multi(*keys_to_collection_map.keys, options) do |key|
           _scope { yield keys_to_collection_map[key] }
-        end        
+        end
       else
         results = keys_to_collection_map.map do |key, item|
           ::Rails.cache.fetch(key, options) { _scope { yield item } }
         end
       end
-      
+
       _process_collection_results(results)
     else
       array! collection, options, &block
     end
   end
-  
-  
-  
+
+
+
   protected
-  
+
   ## Implementing our own version of _cache_key because jbuilder's is protected
   def _cache_key_fetch_multi(key, options)
     if @context.respond_to?(:cache_fragment_name)
@@ -44,18 +44,18 @@ JbuilderTemplate.class_eval do
       ::ActiveSupport::Cache.expand_cache_key(key.is_a?(::Hash) ? url_for(key).split('://').last : key, :jbuilder)
     end
   end
-  
+
   def _keys_to_collection_map(collection, options)
     key = options.delete(:key)
 
     collection.inject({}) do |result, item|
-      key = key.respond_to?(:call) ? key.call(item) : key
-      cache_key = key ? [key, item] : item
+      item_key = key.respond_to?(:call) ? key.call(item) : key
+      cache_key = item_key ? [item_key, item] : item
       result[_cache_key_fetch_multi(cache_key, options)] = item
       result
     end
   end
-  
+
   def _process_collection_results(results)
     _results = results.class == Hash ? results.values : results
     #support pre 2.0 versions of jbuilder where merge! is still private
@@ -67,5 +67,5 @@ JbuilderTemplate.class_eval do
       _results
     end
   end
-  
+
 end
